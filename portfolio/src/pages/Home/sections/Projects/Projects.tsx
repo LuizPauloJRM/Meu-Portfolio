3986212a4ec2db4af2d905eca793f60224576216
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-04-20 11:39:04 -0300

    update hero

---
+++ portfolio/src/pages/Home/sections/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <p>Bem-vindo à página Projects!</p>
+        </div>
+    );
+};
+
+export default Projects;
---
+++ portfolio/src/pages/Skills.tsx/Skills.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+    return (
+        <div>
+            <h1>Skills</h1>
+            <p>Bem-vindo à página Skills!</p>
+        </div>
+    );
+};
+
+export default Skills;
